66e26ba5038ba5793108b80b0d30a01f18526b8f
arvinxx <arvinx@foxmail.com>
2025-01-02 21:13:50 +0800

    Update sql

--- src/database/migrations/0014_curvy_toad.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "ai_providers" RENAME COLUMN "config" TO "settings";--> statement-breakpoint
-ALTER TABLE "ai_providers" DROP COLUMN "sdk_type";--> statement-breakpoint
-ALTER TABLE "ai_providers" DROP COLUMN "enabled_chat_models";
